2a6006a663391dad467d9ae8194023e083ba592e
Marcos Cruz Rojas <cruzrojasmarcos4@gmail.com>
2025-11-14 14:45:07 -0600

    pequeño cambio, esque soy bien vulgar 😿

--- si.py
+++ si.py
@@ -1,7 +1,2 @@
-def edad(age: int):
-    if age > 50:
-        print('Sos un Lentejo, es decir, LENTo y pendEJO XD😹🫵')
-    else:
-        print("piola 🗿👍")
-
-edad(98)
+helloworld = 'print'
+print(helloworld)
